6bf6562984d6d97110045dce273a2256bbb6f696
santi4ag022 <santiago.torrico.1@ucb.edu.bo>
2024-08-12 19:55:34 -0400

    Correccion 1

--- BLINK/BLINK.ino
+++ BLINK/BLINK.ino
@@ -1,13 +1,13 @@
- #define num 1
+ #define LED 13
+ #define time 1000
 void setup() {
-  // put your setup code here, to run once:
 pinMode(num, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  digitalWrite(num,HIGH)
-  delay(1000)
-  digitalWrite(num,LOW)
-  delay(1000)
+  //
+  digitalWrite(LED, HIGH);
+  delay(time);
+  digitalWrite(LED, LOW); 
+  delay(time);
 }
