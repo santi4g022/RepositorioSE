c69584bca09e2742b782d92e3575854c421d5f1d
santi4ag022 <santiago.torrico.1@ucb.edu.bo>
2024-08-23 11:38:00 -0400

    Corrección

--- Mis códigos/Clase2/PULSADOR.ino
+++ Mis códigos/Clase2/PULSADOR.ino
@@ -1,21 +1,15 @@
-#define LED 13
-#define BUTTON 2
+#define LED 2
+#define PUL 4
 
-void setup() {
-  pinMode(LED, OUTPUT);
-  pinMode(BUTTON, INPUT);
+void setup(){
+  pinMode(LED,OUTPUT);
+  pinMode(PUL,INPUT);
 }
 
-void loop() {
-  /*Se crea la variable "dato" para leer si hay un true o un false en 
-  el puerto digital nro 2, esto cambiará con el pulsado del botón*/
-  byte dato = digitalRead(BUTTON);
-  if (dato==1){
-    /*Si es true, el led se encenderá*/
-    digitalWrite(LED, HIGH);
+void loop(){
+  if(digitalRead(PUL)){
+    digitalWrite(LED,1);
   }else{
-    /*Si es false, el led se apagará, respondiendo de esta manera
-    al pulso del botón.*/
-    digitalWrite(LED, LOW);
+    digitalWrite(LED,0);
   }
 }
