743e6daf019708e6a8c978aaa41f0440418a0975
santi4ag022 <santiago.torrico.1@ucb.edu.bo>
2024-08-26 19:12:45 -0400

    Cambio

---
+++ Mis códigos/Clase2/T4 PWM.ino
@@ -0,0 +1,32 @@
+#define POT A5
+#define DELAYTIME 100
+#define BAUTRATE 9600
+#define LED 6
+float lectura;
+float conv;
+int pwm;
+
+void setup(){
+  /*Se inicia el monitor serial con el siguiente comando, 9600 es la 
+  resolución ideal de comunicación entre el arduino y el monitor serial*/
+  pinMode(POT,INPUT);
+  pinMode(LED,OUTPUT);
+  Serial.begin(BAUTRATE);
+}
+
+void loop(){
+  /*La variable float "lectura" nos va a proporcionar la información
+  obtenida en el pin A5, que va de 0 a 1023 por su resolución*/
+  lectura = analogRead(POT);
+  /*Con la siguiente linea se hace una conversión de la lectura a V,
+  para saber el voltaje de salida del potenciometro*/
+  conv = (lectura*5)/1023;
+  /*Se realiza la impresión de la lectura convertida en el mon. serial*/
+  Serial.print("Voltaje: ");
+  Serial.print(conv);
+  Serial.println(" V.");
+  /*Se introduce un delay para desacelerar las lecturas por segundo*/
+  pwm = map(lectura,0,1023,0,254);
+  delay(DELAYTIME);
+  analogWrite(LED,pwm);
+}
