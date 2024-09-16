55cccc69a81e0c8508005e9e314fb5bcd1548c68
santi4g022 <santiago.torrico.1@ucb.edu.bo>
2024-09-16 19:48:37 -0400

    Update T  CONTROLVOL.ino

--- Mis códigos/Clase2/T  CONTROLVOL.ino
+++ Mis códigos/Clase2/T  CONTROLVOL.ino
@@ -1,3 +1,4 @@
+//DEFINICIÓN DE MACROS
 #include <EEPROM.h>
 #define BOTONMAS 3
 #define BOTONMENOS 2
@@ -6,45 +7,68 @@
 #define VOLMIN 0
 #define CELDA 0
 #define TIME 5000
-
+#define TIMESEP 50
+//DEFINICIÓN DE VARIABLES
 uint8_t volumen = EEPROM.read(CELDA);
 volatile unsigned long lasttime = 0;
-volatile unsigned long debounceDelay = 60;
-
-void setup(){
-    pinMode(BOTONMAS,INPUT_PULLUP);
-    pinMode(BOTONMENOS,INPUT_PULLUP);
-    attachInterrupt(digitalPinToInterrupt(BOTONMAS),aumentar,FALLING);
-    attachInterrupt(digitalPinToInterrupt(BOTONMENOS),disminuir,FALLING);
-    Serial.begin(BAUDRATE);
+volatile unsigned long debounceDelay = 80;
+volatile bool pinMenos = false;
+volatile bool pinMas = false;
+//DEFINICIÓN DE PINES E INTERRUPCIONES
+void setup() {
+  pinMode(BOTONMAS, INPUT_PULLUP);
+  pinMode(BOTONMENOS, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(BOTONMAS), aumentar, RISING);
+  attachInterrupt(digitalPinToInterrupt(BOTONMENOS), disminuir, RISING);
+  Serial.begin(BAUDRATE);
 }
-
-void loop(){
-    delay(TIME);
-}
-
-void aumentar(){
-    if(millis() - lasttime > debounceDelay){
-        if(volumen<VOLMAX){
-            volumen++;
-            Serial.print("Volumen: ");
-            Serial.print(volumen);
-            Serial.println(".");
-            EEPROM.write(CELDA,volumen);
-        }
-        lasttime = millis();
+void loop() {
+  //LÓGICA PARA AUMENTO DE VOLUMEN
+  if (pinMas == true && pinMenos == false) {
+    if (volumen < VOLMAX) {
+      volumen++;
+      Serial.print("Volumen: ");
+      Serial.print(volumen);
+      Serial.println(".");
+      EEPROM.write(CELDA, volumen);
     }
-}
-
-void disminuir(){
-    if(millis() - lasttime > debounceDelay){
-        if(volumen>VOLMIN){
-            volumen--;
-            Serial.print("Volumen: ");
-            Serial.print(volumen);
-            Serial.println(".");
-            EEPROM.write(CELDA,volumen);
-        }
-        lasttime = millis();
+    pinMas = false;
+  }
+  delay(TIMESEP);
+  //LÓGICA PARA DISMINUCIÓN DE VOLUMEN 
+  if (pinMenos == true && pinMas == false) {
+    if (volumen > VOLMIN) {
+      volumen--;
+      Serial.print("Volumen: ");
+      Serial.print(volumen);
+      Serial.println(".");
+      EEPROM.write(CELDA, volumen);
     }
+    pinMenos = false;
+  }
+  delay(TIMESEP);
+  //LÓGICA PARA REINICIAR VOLUMEN 
+  if(pinMas == true && pinMenos == true){
+    volumen = 0;
+    Serial.print("Volumen: ");
+      Serial.print(volumen);
+      Serial.println(".");
+      EEPROM.write(CELDA, volumen);
+    pinMas = false;
+    pinMenos = false;
+  }
+  delay(TIMESEP);
+}
+//INTERRUPCIONES PARA ACTIVAR BANDERAS
+void aumentar() {
+  if (millis() - lasttime > debounceDelay) {
+    pinMas = true;
+    lasttime = millis();
+  }
+}
+void disminuir() {
+  if (millis() - lasttime > debounceDelay) {
+    pinMenos = true;
+    lasttime = millis();
+  }
 }
